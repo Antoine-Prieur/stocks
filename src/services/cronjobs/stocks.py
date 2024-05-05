edc13782d2009b05c908c7ea15ecbffc8dca7ba6
antoine-prieur1 <antoine.prieur@celliersintelligents.com>
2024-05-05 19:11:24 -0400

    initial commit

---
+++ src/services/cronjobs/stocks.py
@@ -0,0 +1,22 @@
+from enum import Enum
+from typing import Literal
+
+
+class TickersEnum(Enum):
+    MSFT = "Microsoft"
+    AAPL = "Apple"
+    NVDA = "Nvidia"
+    GOOGL = "Google"
+    GOOG = "Google"
+    AMZN = "Amazon"
+    META = "Meta"
+    TSM = "Taiwan Semiconductor Manufacturer"
+    AMD = "AMD"
+    QCOM = "QUALCOMM Incorporated"
+    TXN = "Texas Instruments Incorporated"
+    IBM = "IBM"
+    INTC = "Intel"
+
+
+def get_stocks_from_ticker():
+    pass
